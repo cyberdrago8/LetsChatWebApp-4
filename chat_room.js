06c7867996be49f7401c49a4dbc1ed1c88b2f8a5
cyberdrago8 <53197677+cyberdrago8@users.noreply.github.com>
2022-08-05 14:09:20 -0700

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,46 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCH8DtD_eW4m5zGkWAohw3VV4pW1x6iX6w",
+    authDomain: "lets-chat-web-app-2-3f58f.firebaseapp.com",
+    databaseURL: "https://lets-chat-web-app-2-3f58f-default-rtdb.firebaseio.com",
+    projectId: "lets-chat-web-app-2-3f58f",
+    storageBucket: "lets-chat-web-app-2-3f58f.appspot.com",
+    messagingSenderId: "288445491314",
+    appId: "1:288445491314:web:02d899c505a61da3663aeb"
+  };
+
+firebase.initializeApp(firebaseConfig);
+
+
+function addRoom()
+{
+    room_name = document.getElementById("room_name").value;
+    firebase.database().ref("/").child(room_name).update({
+        purpose : "adding room"
+    });
+    localStorage.setItem("room_name", room_name);
+
+    window.location = "chat_room.html";
+}
+function getData() {firebase.database().ref("/").on('value',
+function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey = childSnapshot.key;
+    Room_names = childKey;
+    //Start code
+    row = "<div class='Room_names' id="+Room_names+" onclick='redirectToRoomName(kwitter_page.html)'>#"+Room_names+"</div></hr>";
+    //End code
+    });});}
+    getData();
+
+function logout(){
+    onclick="redirectToRoomName(index.html)"
+}
+
+function send(){
+    firebase.database().ref(room_name).push({
+        name:user_name,
+        message:msg,
+        like:0
+    });
+}
+
+
+
